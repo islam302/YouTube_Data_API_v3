e7568a9669d232d4437180a2c778acc871bd13a6
islam302 <islambadran39@gmail.com>
2024-11-05 20:01:11 +0200

    gmail-api fetch data

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import extract_emails
+
 
 urlpatterns = [
     path('test_video_info/', views.get_video_info, name='test_video_info'),  # Change the path
+    path('google/', extract_emails, name='extract_emails'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,7 @@
-import requests
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-import json
+
+
 
 API_KEY = 'AIzaSyA20esOYFWy1cxHVdB-m7bTAzrwnBKkmNc'
-
 
 def extract_video_id(url):
     if 'youtu.be' in url:
@@ -42,3 +39,5 @@ def get_video_info(request):
             return JsonResponse({"error": "Video not found"}, status=404)
     else:
         return JsonResponse({"error": "Invalid request method"}, status=400)
+
+
